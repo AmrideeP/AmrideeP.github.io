5e52caef9a9d2205d150d022e1a6c43fccdc178b
AmrideeP <abaksi57@gmail.com>
2022-01-11 00:02:46 +0530

    Projects

--- script.js
+++ script.js
@@ -12,4 +12,26 @@ $(document).ready(function () {
     $(".menu-btn svg").toggleClass("fa-bars");
     $(".menu-btn svg").toggleClass("fa-times-circle");
   });
+
+  // Owl Carousel Script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 1000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
 });
