9532fefc5c4253560b2d37c9f477cbfa5057d881
AmrideeP <abaksi57@gmail.com>
2022-01-13 02:02:51 +0530

    Contact

--- script.js
+++ script.js
@@ -35,3 +35,39 @@ $(document).ready(function () {
     },
   });
 });
+
+async function sendEmail(e) {
+  $("body").waitMe({
+    effect: "bounce",
+    text: "Please Wait! You will receive an email shortly.",
+    maxSize: "",
+    waitTime: -1,
+    textPos: "vertical",
+    fontSize: "25px",
+    source: "",
+    onClose: function () {},
+  });
+  e.preventDefault();
+  const data = new FormData(e.target);
+  const value = Object.fromEntries(data.entries());
+  let message = await emailjs.send("service_32jfnbm", "template_jxcjjbr", {
+    from_name: "Amrideep Baksi",
+    to_name: value.name,
+    to_email: value.email,
+  });
+  await emailjs.send("service_32jfnbm", "template_fa4u0hu", {
+    from_name: value.name,
+    message: value.message,
+    from_email: value.email,
+  });
+  $("body").waitMe("hide");
+  if (message.text == "OK") {
+    message = "Email sent successfully.";
+  } else {
+    message = message.text;
+  }
+  alert(message);
+  document.getElementById("contactForm").reset();
+}
+
+document.getElementById("contactForm").addEventListener("submit", sendEmail);
